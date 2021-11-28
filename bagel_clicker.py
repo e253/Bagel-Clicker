f88b654a6aa346ca276b3411b36f10636fbe0efc
e253 <62357808+e253@users.noreply.github.com>
2021-11-28 18:54:40 -0500

    Basic Movement added.

--- bagel_clicker.py
+++ bagel_clicker.py
@@ -1,3 +1,44 @@
 import pygame
 import gamebox
+import random
 
+camera = gamebox.Camera(800,600)
+
+# Bagel
+bagel = gamebox.from_image(400, 300, "bagel.png")
+bagel.size = [20,15]
+# Borders
+bottom = gamebox.from_color(400, 601, "white", 800, 1)
+top = gamebox.from_color(400, -1, "white", 800, 1)
+left = gamebox.from_color(-1, 300, "white", 1, 600)
+right = gamebox.from_color(801, 300, 'white', 1, 600)
+
+
+bagel.speedx = random.randrange(-10, 10)
+bagel.speedy = random.randrange(-10, 10)
+    
+
+
+ticks = 0
+def tick(keys):
+
+
+    # Side Interactions
+    if bagel.touches(bottom) or bagel.touches(top):
+        bagel.speedy *= -1
+    if bagel.touches(left) or bagel.touches(right):
+        bagel.speedx *= -1
+    
+
+    # Move Bagel Around
+    bagel.x += bagel.speedx
+    bagel.y += bagel.speedy
+
+    camera.clear('black')
+    camera.draw(bagel)
+    camera.display()
+
+
+
+ticks_per_second = 30
+gamebox.timer_loop(ticks_per_second, tick)
