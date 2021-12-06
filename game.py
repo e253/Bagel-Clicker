7586ff9914c1930fe8ae30c70f1368feb99853fd
ean4tp <94562394+ean4tp@users.noreply.github.com>
2021-12-06 15:54:18 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,64 @@
+# Names: Emilie Neulen, Ethan Steere
+# Computing IDs: ean4tp, xaq5rz
+# Overall Description
+'''
+https://github.com/e253/Bagel-Clicker-Game/blob/master/bagel_clicker.py
+Bodo’s bagel unofficially sponsered game.
+Object of the game is to click on an bagel that moves .
+"Orders" come in for each level for a certain amount of bagels. The player must click on the moving bagel to make a bagel.
+This becomes increasingly more difficult as the level number gets higher as there are more bagels that need to be made.
+'''
+#####################
+# Required Features #
+#####################
 
+# 1: User Input
+"""
+There is user input for the main action from the mouse. The mouse must try to click on the bagel.
+"""
+
+# 2: Start Screen
+"""
+There will be a start screen where the player can opt to start the game.
+"""
+
+# 3: Game Over
+"""
+The condition for ending the game will be the failing to complete an order in the allotted time. The bagel will stop moving and there will be a "GAME OVER" message.
+"""
+
+# 4: Small Enough Window
+"""
+The window size will be 800 by 600, the maximum allowed size. 
+"""
+
+# 5: Graphics and Images
+"""
+There will be a bagel graphic as well as a background of an industrial restaurant kitchen where our player is theoretically working.
+"""
+
+
+
+######################
+# 4 Optional Featues #
+######################
+
+# 1: Timer
+"""
+There will be a timer to make the required amount of bagels to fufill the order.
+"""
+
+# 2: Multiple Levels
+"""
+As the player compeletes each order they go up a level. With each new level the game gets more difficult....
+"""
+
+# 3: Restart from Game Over
+"""
+When the player inevitably loses, they can restart by hitting the Space key when prompted.
+"""
+
+# 4: Sprite Animation
+"""
+When you click on the bagel it will deform outward slightly to let you know you successfully pressed it. The animation will make it seem as if you really squish the bagel.
+"""
